ec8c501dfda293559c6aaf774aa26381343f32a7
henrique <henriquelijo@gmail.com>
2021-03-17 17:12:47 +0100

    mercores

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import './Rooms.scss';
-import room1 from '../../assets/img/habitaciones/room1.jpg';
 
 
 export function Rooms () {  
  
 
     return (  
-            <div>  
+            <div className="container-ourrooms">  
 
-            <h2 className="housecontainer-ourhistory__title rooms-title">
+            <h2 className="container-ourrooms__title">
                 Habitaciones
             </h2>  
-                <div className="housecontainer-ourhistory__textimg__story">
+            <div className="container-ourrooms__text">
                
-               <p className="housecontainer-ourhistory__textimg__story-1">
-               Casa A Pasada dispone de cinco amplias habitaciones dobles (convertibles en triples o cuadruples), con baño completo y televisión. 
+               <p className="container-ourrooms__text-1">
+               Casa A Pasada dispone de cinco amplias habitaciones dobles
+                (convertibles en triples o cuadruples),
+                con baño completo y televisión. 
 
-               Cada una de ellas lleva el nombre de lugares de la zona (Cedeira, Ortigueira, Cariño y Cabo Ortegal) y el del antiguo uso de la casa (‘Escola das Nenas’).
+               Cada una de ellas lleva el nombre de lugares de la zona 
+               (Cedeira, Ortigueira, Cariño y Cabo Ortegal) y el del antiguo
+                uso de la casa (‘Escola das Nenas’).
                </p>
             
            </div>
+           
            <div className="imgContainer2"></div>
-           <div className="prizes">
+           <div className="container-ourrooms__prizes">
                
                
-            <h3 className="">
+            <h3 className="container-ourrooms__prizes-title">
                 Tarifas
             </h3>
                <p> Habitación doble con desayuno (IVA Incluido)</p>
